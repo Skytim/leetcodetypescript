8e14baf297a4528a9508dff6f21b2247f8c7c7dc
Tim Tsai <sky22357168@gmail.com>
2023-07-23 11:19:20 +0800

    Init Comment

---
+++ 1.two-sum.ts
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=1 lang=typescript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+    const map = new Map<number, number>();
+    for (let i = 0; i < nums.length; i++) {
+        const x = target - nums[i];
+        if (map.has(x)) {
+
+            return [map.get(x)!, i];
+        } else {
+            map.set(nums[i], i);
+        }
+
+    }
+    return [];
+};
+// @lc code=end
+
