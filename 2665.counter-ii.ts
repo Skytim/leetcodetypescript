ada4486d6f522bf8adf65ae453ab5902ae98e4d8
SkyTim <sky22357168@gmail.com>
2023-08-24 18:20:41 +0800

    [2665] Counter II

---
+++ 2665.counter-ii.ts
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=2665 lang=typescript
+ *
+ * [2665] Counter II
+ */
+
+// @lc code=start
+type ReturnObj = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): ReturnObj {
+    let n = init;
+    return {
+        increment: () => {
+            return ++n;
+        },
+        decrement: () => {
+            return --n;
+        },
+        reset: () => {
+            n = init;
+            return n;
+        }
+    }
+};
+// @lc code=end
+
+
+
+const counterII = createCounter(5)
+
+console.log(counterII.increment()); // 6
+console.log(counterII.reset()); // 5
+console.log(counterII.decrement()); // 4
