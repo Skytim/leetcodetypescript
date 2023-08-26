77f6a5db3919270d2643f698ddd0c4ff0d8551e6
SkyTim <sky22357168@gmail.com>
2023-08-26 13:43:08 +0800

    Hello Typescript

---
+++ scratch-plan/1.hello-typescript.ts
@@ -0,0 +1,14 @@
+// function sayHello(person: string) {
+//     return 'Hello, ' + person;
+// }
+
+// let user = 'Tom';
+// console.log(sayHello(user));
+// Have Error
+function sayHello(person: string) {
+    return 'Hello, ' + person;
+}
+
+let user = [0, 1, 2];
+console.log(sayHello(user));
+
