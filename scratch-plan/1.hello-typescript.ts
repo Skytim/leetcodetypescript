d7e94e3332b6b605e959498e397923eb266ab1bf
SkyTim <sky22357168@gmail.com>
2023-08-26 17:49:07 +0800

    Day 1

--- scratch-plan/1.hello-typescript.ts
+++ scratch-plan/1.hello-typescript.ts
@@ -1,14 +1,15 @@
-// function sayHello(person: string) {
-//     return 'Hello, ' + person;
-// }
+namespace HelloTypescript {
+    // function sayHello(person: string) {
+    //     return 'Hello, ' + person;
+    // }
 
-// let user = 'Tom';
-// console.log(sayHello(user));
-// Have Error
-function sayHello(person: string) {
-    return 'Hello, ' + person;
+    // let user = 'Tom';
+    // console.log(sayHello(user));
+    // Have Error
+    function sayHello(person: string) {
+        return "Hello, " + person;
+    }
+
+    let user = [0, 1, 2];
+    console.log(sayHello(user));
 }
-
-let user = [0, 1, 2];
-console.log(sayHello(user));
-
