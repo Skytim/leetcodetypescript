bc916e348865aabd42ac74a316df3d49201dc0ad
SkyTim <sky22357168@gmail.com>
2023-08-26 17:49:03 +0800

    Day 2

--- scratch-plan/2.2.any.ts
+++ scratch-plan/2.2.any.ts
@@ -1,24 +1,25 @@
-// let myFavoriteNumber: string = 'seven';
-// myFavoriteNumber = 7;
+namespace Any {
+    // let myFavoriteNumber: string = 'seven';
+    // myFavoriteNumber = 7;
 
-// index.ts(2,1): error TS2322: Type 'number' is not assignable to type 'string'.
+    // index.ts(2,1): error TS2322: Type 'number' is not assignable to type 'string'.
 
-let myFavoriteNumber: any = 'seven';
-myFavoriteNumber = 7;
+    let myFavoriteNumber: any = "seven";
+    myFavoriteNumber = 7;
 
+    // let anyThing: any = 'hello';
+    // console.log(anyThing.myName);
+    // console.log(anyThing.myName.firstName);
 
-// let anyThing: any = 'hello';
-// console.log(anyThing.myName);
-// console.log(anyThing.myName.firstName);
+    // let anyThing: any = 'Tom';
+    // anyThing.setName('Jerry');
+    // anyThing.setName('Jerry').sayHello();
+    // anyThing.myName.setFirstName('Cat');
+    // console.log(anyThing.myName.firstName);
 
-// let anyThing: any = 'Tom';
-// anyThing.setName('Jerry');
-// anyThing.setName('Jerry').sayHello();
-// anyThing.myName.setFirstName('Cat');
-// console.log(anyThing.myName.firstName);
+    let something;
+    something = "seven";
+    something = 7;
 
-let something;
-something = 'seven';
-something = 7;
-
-// something.setName('Tom');
+    // something.setName('Tom');
+}
