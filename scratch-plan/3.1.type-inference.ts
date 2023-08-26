740218999fe7978544775bc295a5b89723bce52d
SkyTim <sky22357168@gmail.com>
2023-08-26 17:48:56 +0800

    Day 3

---
+++ scratch-plan/3.1.type-inference.ts
@@ -0,0 +1,9 @@
+namespace TypeInference {
+    let myFavoriteNumber = "seven";
+    myFavoriteNumber = 7;
+    // index.ts(2,1): error TS2322: Type 'number' is not assignable to type 'string'.
+
+    let myFavoriteNumber2;
+    myFavoriteNumber2 = "seven";
+    myFavoriteNumber2 = 7;
+}
---
+++ scratch-plan/3.2.union-types.ts
@@ -0,0 +1,24 @@
+namespace UnionTypes {
+    let myFavoriteNumber: string | number;
+    myFavoriteNumber = "seven";
+    myFavoriteNumber = 7;
+
+    let myFavoriteNumber2: string | number;
+    myFavoriteNumber2 = true;
+
+    function getLength(something: string | number): number {
+        return something.length;
+    }
+
+    function getString(something: string | number): string {
+        return something.toString();
+    }
+
+    let myFavoriteNumber3: string | number;
+    myFavoriteNumber3 = "seven";
+    console.log(myFavoriteNumber3.length); // 5
+    myFavoriteNumber3 = 7;
+    console.log(myFavoriteNumber3.length); // 編譯時報錯
+    // index.ts(5,30): error TS2339: Property 'length' does not exist on type 'number'.
+
+}
