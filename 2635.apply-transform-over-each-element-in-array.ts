b4c885a64ca2c4862dc5cceb19067803aa0711d4
SkyTim <sky22357168@gmail.com>
2023-08-26 10:35:47 +0800

    [2635] Apply Transform Over Each Element in Array

--- 2635.apply-transform-over-each-element-in-array.ts
+++ 2635.apply-transform-over-each-element-in-array.ts
@@ -6,11 +6,45 @@
 
 // @lc code=start
 export function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-    let result: number[] = [];
-    arr.forEach((item, index) => {
-        result.push(fn(item, index));
-    });
-    return result;
+    /// Method: 1
+    // let result: number[] = [];
+    // let result2: Array<number> = [];
+    // let result3 = new Int32Array(arr.length);
+    // for (let i = 0; i < arr.length; ++i) {
+    //     result[i] = fn(arr[i], i);
+    // }
+    // return result;
+
+    // /// Method: 2
+    // result = new Array(arr.length);
+    // for (const i in arr) {
+    //     result[i] = fn(arr[i], Number(i));
+    // }
+    // return result;
+
+    // /// Method: 3
+    // result = [];
+    // for (let i = 0; i < arr.length; ++i) {
+    //     result.push(fn(arr[i], i));
+    // }
+    // return result;
+    // /// Method: 4
+    // result2 = new Array<number>(arr.length);
+    // for (let i = 0; i < arr.length; ++i) {
+    //     result2[i] = fn(arr[i], i);
+    // }
+    // return result2;
+    // /// Method: 5
+    // result3 = new Int32Array(arr.length);
+    // for (let i = 0; i < arr.length; ++i) {
+    //     result3[i] = fn(arr[i], i);
+    // }
+    // return result3 as unknown as number[];
+    /// Method: 6
+    for (let i = 0; i < arr.length; ++i) {
+        arr[i] = fn(arr[i], i);
+    }
+    return arr;
 }
 // @lc code=end
 
