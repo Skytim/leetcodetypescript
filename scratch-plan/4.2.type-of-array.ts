a30a75bcf2c51301a2437c1af4bcf2d7c1d5ac8c
SkyTim <sky22357168@gmail.com>
2023-08-27 09:47:33 +0800

    Day 4

---
+++ scratch-plan/4.2.type-of-array.ts
@@ -0,0 +1,38 @@
+namespace TypeOfArray {
+    let fibonacci: number[] = [1, 1, 2, 3, 5];
+    let fibonacci2: number[] = [1, "1", 2, 3, 5];
+
+    let fibonacci3: number[] = [1, 1, 2, 3, 5];
+    fibonacci3.push("8");
+
+    let fibonacci4: Array<number> = [1, 1, 2, 3, 5];
+
+    interface NumberArray {
+        [index: number]: number;
+    }
+    let fibonacci5: NumberArray = [1, 1, 2, 3, 5];
+
+    function sum() {
+        let args: number[] = arguments;
+    }
+
+    function sum2() {
+        let args: {
+            [index: number]: number;
+            length: number;
+            callee: Function;
+        } = arguments;
+    }
+
+    function sum() {
+        let args: IArguments = arguments;
+    }
+
+    interface IArguments {
+        [index: number]: any;
+        length: number;
+        callee: Function;
+    }
+
+    let list: any[] = ["xcatliu", 25, { website: "http://xcatliu.com" }]; 
+}
