bb8df0ace084196050eb0b6652c64a4f6b104c9e
SkyTim <sky22357168@gmail.com>
2023-08-28 22:43:26 +0800

    Day 7

---
+++ scratch-plan/7.1.type-aliases.ts
@@ -0,0 +1,12 @@
+module typeAliases {
+    type Name = string;
+    type NameResolver = () => string;
+    type NameOrResolver = Name | NameResolver;
+    function getName(n: NameOrResolver): Name {
+        if (typeof n === "string") {
+            return n;
+        } else {
+            return n();
+        }
+    }
+}
---
+++ scratch-plan/7.2.string-literal-types.ts
@@ -0,0 +1,11 @@
+module StringLiteralTypes {
+    type EventNames = "click" | "scroll" | "mousemove";
+    function handleEvent(ele: Element | null, event: EventNames) {
+        // do something
+    }
+
+    handleEvent(document.getElementById("hello"), "scroll"); // 沒問題
+    handleEvent(document.getElementById("world"), "dbclick"); // 報錯，event 不能為 'dbclick'
+
+    // index.ts(7,47): error TS2345: Argument of type '"dbclick"' is not assignable to parameter of type 'EventNames'.
+}
