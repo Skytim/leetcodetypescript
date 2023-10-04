940bbfdcb6ef7daf204798d4e485e742a0ea2e5b
SkyTim <sky22357168@gmail.com>
2023-10-04 22:56:37 +0800

    Array Reduce Transformation

--- 2626.array-reduce-transformation.ts
+++ 2626.array-reduce-transformation.ts
@@ -19,11 +19,19 @@ function reduce(nums: number[], fn: Fn, init: number): number {
         accumulator = fn(accumulator, element);
     }
     /// Approach 2: Array.forEach Loop
-    nums.forEach((element) => {
-        accumulator = fn(accumulator, element);
-    });
+    // nums.forEach((element) => {
+    //     accumulator = fn(accumulator, element);
+    // });
 
     return accumulator;
 
-
+    // 1. 陣列中所有的值相加
+    // 2. 根據鍵值建立索引（常見）
+    // 3. 結合 Array.filter 和 Array.map 效能較高
 };
+
+
+const nums = [1, 2, 3];
+
+const sum = reduce(nums, (a, b) => a + b, 0);
+console.log(sum); // 6
