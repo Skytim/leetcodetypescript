750221e64bcb2ec625b04c2f1a552bcce04bbd4e
SkyTim <sky22357168@gmail.com>
2023-10-10 20:29:28 +0800

    Function Composition

--- Day1/2629.function-composition.ts
+++ Day1/2629.function-composition.ts
@@ -8,14 +8,27 @@
 type F = (x: number) => number;
 
 function compose(functions: F[]): F {
-    const reversedFns = [...functions];
-    reversedFns.reverse();
-	return function(x) {
-        let result  = x;
-        for (const fn of reversedFns) {
-            result = fn(result);
-        }
-        return result
+    // const reversedFns = [...functions];
+    // reversedFns.reverse();
+    // return function(x) {
+    //     let result  = x;
+    //     for (const fn of reversedFns) {
+    //         result = fn(result);
+    //     }
+    //     return result
+    // }
+
+    /// Approach 1
+    // return function (x) {
+    //     let result = x;
+    //     for (let i = functions.length - 1; i >= 0; i--) {
+    //       result = functions[i](result);
+    //     }
+    //     return result;
+    //   };
+    /// Approach 2
+    return function (x: number) {
+        return functions.reduceRight((acc, fn) => fn(acc), x);
     }
 };
 
