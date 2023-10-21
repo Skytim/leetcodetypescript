6bb6ab437881b3cdea55c06e4ade812e5ee0842e
SkyTim <sky22357168@gmail.com>
2023-10-21 17:00:07 +0800

    Nested Array Generator

---
+++ Day4/2649.nested-array-generator.ts
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=2649 lang=typescript
+ *
+ * [2620] Nested Array Generator
+ */
+
+// @lc code=start
+module NestedArrayGenerator {
+    type MultidimensionalArray = (MultidimensionalArray | number)[]
+
+    function* inorderTraversal(arr: MultidimensionalArray): Generator<number, void, unknown> {
+        if (!Array.isArray(arr)) {
+            yield arr;
+            return;
+        }
+        for (let i = 0; i < arr.length; i++) {
+            yield* inorderTraversal(arr[i] as MultidimensionalArray);
+        }
+    };
+
+
+    const gen = inorderTraversal([1, [2, 3]]);
+    gen.next().value; // 1
+    gen.next().value; // 2
+    gen.next().value; // 3
+    gen.next().done; // true
+
+
+}
+// @lc code=end
