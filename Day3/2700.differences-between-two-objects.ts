6d8c9ea26d404e0fc3f532808a2b6a63f3f35c6e
SkyTim <sky22357168@gmail.com>
2023-10-22 22:36:44 +0800

    DifferencesBetweenTwoObjects

---
+++ Day3/2700.differences-between-two-objects.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=2700 lang=typescript
+ *
+ * [2620] Differences Between Two Objects
+ */
+
+// @lc code=start
+module DifferencesBetweenTwoObjects {
+    function objDiff(obj1: any, obj2: any): any {
+        if (obj1 === obj2) return {};
+        if (obj1 === null || obj2 === null) return [obj1, obj2];
+        if (typeof obj1 !== 'object' || typeof obj2 !== 'object') return [obj1, obj2];
+        if (Array.isArray(obj1) !== Array.isArray(obj2)) return [obj1, obj2];
+
+        const returnObject: any = {};
+        for (const key in obj1) {
+            if (key in obj2) {
+                const subDiff = objDiff(obj1[key], obj2[key]);
+                if (Object.keys(subDiff).length > 0) {
+                    returnObject[key] = subDiff;
+                }
+            }
+        }
+        return returnObject;
+    }
+}
+// @lc code=end
