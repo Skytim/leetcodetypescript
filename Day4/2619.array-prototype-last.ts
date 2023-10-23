02aad09de7f1a1c970640a03c7c12f110c3ed131
Tim.Tsai <tim.tsai@deltaww.com>
2023-10-23 14:03:30 +0800

    Array Prototype Last

---
+++ Day4/2619.array-prototype-last.ts
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=2619 lang=typescript
+ *
+ * [2625] Array Prototype Last
+ */
+
+// @lc code=start
+
+declare global {
+    interface Array<T> {
+        last(): T | -1;
+    }
+}
+
+Array.prototype.last = function () {
+    if(!this.length) {
+        return -1;
+    }
+    return this[this.length - 1];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
+
+export {};
+
+// @lc code=end
